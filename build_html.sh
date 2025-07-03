9888e824518239b9392e6eccd1d0aa0c4ad9bf2a
cnrmurphy <conor.murphy4304@gmail.com>
2025-07-03 13:58:43 -0400

    use awk to build html files

--- build_html.sh
+++ build_html.sh
@@ -2,21 +2,56 @@
 set -e
 
 SRC_DIR="pages"
-OUT_DIR="dist"
+OUT_DIR="public"
 
-# Find all .md files in pages/, recursively
+rm -rf ./public
+mkdir ./public
+
+generate_complete_page() {
+    local mdfile="$1"
+    local outfile="$2"
+    local title="$3"
+    
+    # Convert markdown to HTML and save to temp file
+    local temp_content=$(mktemp)
+    pandoc "$mdfile" > "$temp_content"
+    
+    # Use a more robust approach with awk to handle multiline content
+    awk -v title="$title" -v content_file="$temp_content" '
+    {
+        if ($0 ~ /{{\.Title}}/) {
+            gsub(/{{\.Title}}/, title)
+        }
+        if ($0 ~ /{{printf "%s" \.Content}}/) {
+            while ((getline line < content_file) > 0) {
+                print line
+            }
+            close(content_file)
+            next
+        }
+        print
+    }' wrapper.html > "$OUT_DIR/$outfile"
+    
+    rm "$temp_content"
+}
+
 find "$SRC_DIR" -name '*.md' | while read -r mdfile; do
-    # Strip the source directory prefix
     relpath="${mdfile#$SRC_DIR/}"
     
-    # Change extension to .html
     outfile="${relpath%.md}.html"
 
-    # Ensure destination directory exists
     mkdir -p "$(dirname "$OUT_DIR/$outfile")"
 
-    # Convert Markdown to HTML
-    pandoc "$mdfile" -o "$OUT_DIR/$outfile"
+    filename=$(basename "$mdfile" .md)
+    case "$filename" in
+        "about") title="Conor Murphy" ;;
+        "resume") title="Resume" ;;
+        "contact") title="Conor Murphy" ;;
+        "articles_list") title="Articles" ;;
+        *) title="$filename" ;;
+    esac
+
+    generate_complete_page "$mdfile" "$outfile" "$title"
     
     echo "Generated: $OUT_DIR/$outfile"
 done
