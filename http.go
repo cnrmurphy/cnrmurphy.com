567ea883a9b0753efb02a018ef11f5b3aa35f10e
cnrmurphy <conor.murphy4304@gmail.com>
2025-07-03 13:59:40 -0400

    remove template injection since we will build files when running dev server

--- http.go
+++ http.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"text/template"
 
 	"github.com/rs/zerolog"
 )
@@ -32,62 +31,38 @@ type Page struct {
 }
 
 func HomeHandler(w http.ResponseWriter, req *http.Request) {
-	c, _ := os.ReadFile("./dist/about.html")
-	t, _ := template.ParseFiles("./wrapper.html")
-
-	p := &Page{
-		Title:   "Conor Murphy",
-		Content: c,
-	}
-
-	t.Execute(w, p)
+	c, _ := os.ReadFile("./public/about.html")
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(c)
 }
 
 func ContactHandler(w http.ResponseWriter, req *http.Request) {
-	c, _ := os.ReadFile("./dist/contact.html")
-	t, _ := template.ParseFiles("./wrapper.html")
-
-	p := &Page{
-		Title:   "Conor Murphy",
-		Content: c,
-	}
-
-	t.Execute(w, p)
+	c, _ := os.ReadFile("./public/contact.html")
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(c)
 }
 
 func ResumeHandler(w http.ResponseWriter, req *http.Request) {
-	c, _ := os.ReadFile("./dist/resume.html")
-	t, _ := template.ParseFiles("./wrapper.html")
-
-	p := &Page{
-		Title:   "Resume",
-		Content: c,
-	}
-
-	t.Execute(w, p)
+	c, _ := os.ReadFile("./public/resume.html")
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(c)
 }
 
 func ArticlesHandler(w http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path
-	p := &Page{
-		Title: "Articles",
-	}
-	t, _ := template.ParseFiles("./wrapper.html")
 
 	if path == "/articles" {
-		c, _ := os.ReadFile("./dist/articles_list.html")
-		p.Content = c
-		t.Execute(w, p)
+		c, _ := os.ReadFile("./public/articles_list.html")
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(c)
 		return
 	}
 
 	if strings.HasPrefix(path, "/articles/") {
 		articleName := strings.TrimPrefix(path, "/articles/")
-		c, _ := os.ReadFile("./dist/articles/" + articleName + ".html")
-
-		p.Title = articleName
-		p.Content = c
-		t.Execute(w, p)
+		c, _ := os.ReadFile("./public/articles/" + articleName + ".html")
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(c)
 		return
 	}
 	http.NotFound(w, req)
